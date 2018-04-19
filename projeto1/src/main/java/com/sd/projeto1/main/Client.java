7bf2a073c63749f67b9962249f6a166173cc47f1
William Henrique <whsantonio@gmail.com>
2018-04-18 21:16:12 -0300

    client e servidor em construção

--- projeto1/src/main/java/com/sd/projeto1/main/Client.java
+++ projeto1/src/main/java/com/sd/projeto1/main/Client.java
@@ -3,68 +3,72 @@ package com.sd.projeto1.main;
 import com.sd.projeto1.dao.MapaDao;
 import com.sd.projeto1.model.Mapa;
 import com.sd.projeto1.util.PropertyManagement;
-import com.sd.projeto1.util.Utilidades;
 import java.net.*;
 import java.io.*;
 import java.util.*;
 import java.util.Map.Entry;
 
-public class Client implements Runnable{
+public class Client implements Runnable {
 
     private InetAddress enderecoIP;
     private int port; //porta UDP
     private BufferedReader tecladoUsuario;
     private DatagramSocket socketCliente;
-    
+
     private byte[] outData;
     private byte[] inData;
-    
+
     PropertyManagement pm = new PropertyManagement();
-    
-    Client() throws SocketException, UnknownHostException{
+
+    Client() throws SocketException, UnknownHostException {
         this.enderecoIP = InetAddress.getByName(pm.getAddress());
         this.port = pm.getPort();
         socketCliente = new DatagramSocket();
         tecladoUsuario = new BufferedReader(new InputStreamReader(System.in));
     }
-    
-    
 
     public static void main(String[] args) throws IOException, Exception {
         new Thread(new Client()).start();
-      
+
     }
-    
+
+    @Override
     public void run() {
-	    
-            System.out.println("Cliente Iniciado, esperando por mensagem:");
-		
-            while(true){
+
+        System.out.println("Cliente Iniciado, esperando por mensagem:");
+
+        while (true) {
             try {
-                    inData = new byte[1400];
-                    outData = new byte[1400];
-                   
-                    System.out.print("> ");
-                    String sentence = tecladoUsuario.readLine();
-                    outData = sentence.getBytes();
+                inData = new byte[1400];
+                outData = new byte[1400];
 
-                    DatagramPacket out = new DatagramPacket(outData, outData.length, this.enderecoIP, this.port);
-                    socketCliente.send(out);
+                System.out.print("> ");
+                String sentence = tecladoUsuario.readLine();
+                outData = sentence.getBytes(); // Pegando tamanho da String
+                
+                //Enviando o pacote de datagrama para o servidor
+                DatagramPacket outDatagram = new DatagramPacket(outData, outData.length, this.enderecoIP, this.port);
+                socketCliente.send(outDatagram);
 
-                    DatagramPacket in = new DatagramPacket(inData, inData.length);
-                    socketCliente.receive(in);
+                //recebendo o pacote datagrama do servidor
+                DatagramPacket inDatagram = new DatagramPacket(inData, inData.length);
+                socketCliente.receive(inDatagram);
 
-                    String modifiedSentence = new String(in.getData());
-                    System.out.println("Servidor >" + modifiedSentence);
+                //imprimindo mensagem recebida do servidor;
+                String modifiedSentence = new String(inDatagram.getData());
+                System.out.println("Servidor >" + modifiedSentence);
+                
+                // Close o socket do cliente
+                shutdown();
 
             } catch (IOException e) {
                 System.out.println("Excessão causada: " + e.getLocalizedMessage());
             }
         }
     }
-    
-    private void shutdown(){
-		socketCliente.close();
+
+    private void shutdown() {
+        socketCliente.close();
     }
 
 }
--- projeto1/src/main/java/com/sd/projeto1/main/Server.java
+++ projeto1/src/main/java/com/sd/projeto1/main/Server.java
@@ -1,14 +1,50 @@
 package com.sd.projeto1.main;
 
-
-import com.sd.projeto1.util.Utilidades;
+import com.sd.projeto1.util.PropertyManagement;
 import java.io.*;
 import java.net.*;
 import java.text.SimpleDateFormat;
 import java.util.*;
 
+public class Server implements Runnable {
 
-public class Server {
-	
+    private DatagramSocket socketServidor;
+
+    private byte[] inData;
+    private byte[] outData;
+
+    PropertyManagement pm = new PropertyManagement();
+
+    @Override
+    public void run() {
+        while (true) {
+            try {
+                inData = new byte[1400];
+                outData = new byte[1400];
+
+                // recebendo datagrama do cliente
+                DatagramPacket receivedPacket = new DatagramPacket(inData, inData.length);
+                socketServidor.receive(receivedPacket);
+
+                String text = new String(receivedPacket.getData());
+                outData = text.toUpperCase().getBytes();
+                System.out.println("Mensagem Recebida: " + text);
+
+                // Pegando ip e porta do cliente que enviou o datagram
+                InetAddress IPAddress = receivedPacket.getAddress();
+                int port = receivedPacket.getPort();
+
+                DatagramPacket sendPacket = new DatagramPacket(inData, inData.length, IPAddress, port);
+                socketServidor.send(sendPacket);
+            } catch (IOException e) {
+                System.out.println("Excessão causada: " + e.getLocalizedMessage());
+            }
+
+        }
+    }
+    public static void main(String[] args) throws SocketException{
+            System.out.println("Servidor Iniciado...");
+            new Thread(new Server()).start();
+    }
+
 }
-
